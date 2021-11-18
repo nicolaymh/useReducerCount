065def6c82eeb0256097dd9870fed83cc535aca8
Nicolay Moreno <nicolay.mherrera@gmail.com>
2021-11-17 21:37:03 -0500

    maquetando con bootstrap

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import './App.css';
 
 function App() {
     return (
-        <div className='App'>
-            <h1>Count with Reducer</h1>
+        <div className='text-center'>
+            <h1 className='text-danger'>Count with Reducer</h1>
+            <hr />
+            <h2 className='text-info'>100</h2>
+
+            <div className='d-flex justify-content-center gap-1'>
+                <button className='btn btn-primary'>+1</button>
+                <button className='btn btn-success'>+10</button>
+                <button className='btn btn-warning'>+10</button>
+                <button className='btn btn-success'>-10</button>
+                <button className='btn btn-primary'>-1</button>
+            </div>
         </div>
     );
 }
